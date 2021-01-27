8724b516f79c9b53d8de0de0bfd12838d4ff96c9
mugi <munkhuu1233@gmail.com>
2021-01-26 19:42:47 -0500

    added sytyling

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
